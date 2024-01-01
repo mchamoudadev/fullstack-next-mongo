00455422dad5b5ac46c730460503b39bc38ed29c
mchamoudadev <mchamuuda@gmail.com>
2024-01-02 00:49:57 +0300

    ignored elsint

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import TodoForm from "./_components/todo/TodoForm";
 import Todos from "./_components/todo/Todos";
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <main className='flex items-center justify-center flex-col max-w-lg mx-auto mt-32'>
