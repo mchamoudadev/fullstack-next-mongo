cd809cb34723aa285cb6d38481be7c0060a8612e
mchamoudadev <mchamuuda@gmail.com>
2024-01-02 00:52:02 +0300

    added production url

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,2 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
+export const API_URL = process.env.NODE_ENV === 'production' ? 'fullstack-next-mongo-f5sxnoamc-mchamoudadev.vercel.app'
+    : 'http://localhost:3000';
